7b657f252322340d3d92a704af66b14c973efdfc
Atobaum <atobaum@gmail.com>
2020-07-16 21:42:24 +0900

    finish

---
+++ README.md
@@ -0,0 +1,7 @@
+[Introduction to Rust for Node Developers](https://dev.to/tindleaj/introduction-to-rust-for-node-developers-3j05)를 읽어보고 wc 클론을 만들어보았다.
+
+```bash
+cargo run -- words.txt
+------
+19
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs;
+
+struct PrivateMessage {}
+struct NormalMessage{}
+
+pub trait Message{
+    fn send(&self, content: &str){
+        println!("{}", content);
+    }
+}
+
+impl Message for NormalMessage{}
+
+impl Message for PrivateMessage{
+    fn send(&self, content: &str){ //overiding
+        println!("private: {}", content);
+    }
+}
+
+// enumeration
+enum Coin{
+    Penny,
+    Nickel
+}
+
+
+//pub fn factorial(n: int32){
+//    if(n == 1) {
+//        return 1;
+//    } else {
+//        return n * factorial(n-1);
+//    }
+//}
+
+pub fn main(){
+    let filename = env::args().nth(1).unwrap();
+
+    let file_contents = fs::read_to_string(filename).expect("Error reading file {}");
+
+    let number_of_words = file_contents.split_whitespace().count();
+
+    println!("{}", number_of_words);
 }
